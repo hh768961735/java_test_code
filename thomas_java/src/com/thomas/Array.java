2f6f5dcfaebc7dae0a3148b7e0f2696d061f2215
“thomas” <18434060608@163.com>
2022-04-28 22:18:23 +0800

    java Array

---
+++ thomas_java/src/com/thomas/Array.java
@@ -0,0 +1,41 @@
+package com.thomas;
+
+/**
+ * @Author: Thomas
+ * @Description:
+ * @Date: Created in 22:06 2022/4/28
+ * @Modified By:
+ */
+public class Array {
+
+    public static void main(String[] args) {
+        int size = 10;
+
+        double[] myList = new double[size];
+
+        myList[0] = 5.6;
+        myList[1] = 4.5;
+        myList[2] = 3.3;
+        myList[3] = 13.2;
+        myList[4] = 4.0;
+        myList[5] = 34.33;
+        myList[6] = 34.0;
+        myList[7] = 45.45;
+        myList[8] = 99.993;
+        myList[9] = 99.993;
+
+        double total = 0;
+        // for循环
+        for (int i = 0; i < myList.length; i++) {
+            total += myList[i];
+        }
+        // forEach
+
+        for (double el: myList) {
+            System.out.println(el);
+        }
+
+        System.out.println(total);
+           
+    }
+}
